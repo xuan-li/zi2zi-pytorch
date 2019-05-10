dfaf42bb04b960a0e1dbb5971b637400a7e4cf27
xuan <xuan.shayne.li@gmail.com>
2019-05-10 17:18:50 -0400

    dataset

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .bytesIO import bytes_to_file, PickledImageProvider
+from .image_processing import read_split_image, plot_tensor
---
+++ utils/image_processing.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_split_image(img):
+    box1 = (0,0,img.size[1], img.size[1]) #  (left, upper, right, lower)-tuple
+    box2 = (img.size[1],0, img.size[0], img.size[1])
+    img_A = img.crop(box1)  # target
+    img_B = img.crop(box2)  # source
+    return img_A, img_B
+
+def plot_tensor(tensor):
+    img = np.transpose(tensor.data, (1, 2, 0))
+    print(img.shape)
+    plt.imshow(img)
+
